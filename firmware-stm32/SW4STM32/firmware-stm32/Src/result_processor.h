b48fd4ad5d385a98e72e7e6889d617d773c50e68
Kuba Buda <kubabuda@gazeta.pl>
2017-11-30 23:05:11 +0100

    processing results moved to separate file

---
+++ firmware-stm32/SW4STM32/firmware-stm32/Src/result_processor.h
@@ -0,0 +1,24 @@
+#ifndef _RESULT_PROCESSOR
+#define _RESULT_PROCESSOR
+
+#include "defines.h"
+#include "classifiers_dataset.h"
+
+
+typedef enum MainMode {
+	NN_CLASSIFIER_MODE=0, KNN_CLASSIFIER_MODE=1, RAW_READS_MODE=2, UNIT_TESTS_MODE=3
+} MainMode;
+
+volatile MainMode mode;
+
+
+void process_reads(uint32_t now, classifiers_dataset_t *dataset);
+
+MainMode find_mode(char msg);
+
+void check_mode_switch(void);
+
+int8_t interval_passed(uint32_t now, uint32_t prev, uint32_t interval);
+
+
+#endif // _RESULT_PROCESSOR
--- firmware-stm32/Src/main.c
+++ firmware-stm32/Src/main.c
@@ -36,11 +36,12 @@
   ******************************************************************************
   */
 /* Includes ------------------------------------------------------------------*/
-#include <classifiers_dataset.h>
+#include "classifiers_dataset.h"
 #include "classifiers.h"
 #include "defines.h"
 #include "imu.h"
 #include "main.h"
+#include "result_processor.h"
 #include "stm32f4xx_hal.h"
 #include "tests.h"
 
@@ -49,14 +50,6 @@
 /* USER CODE END Includes */
 
 /* Private variables ---------------------------------------------------------*/
-
-typedef enum MainMode {
-	NN_CLASSIFIER_MODE=0, KNN_CLASSIFIER_MODE=1, RAW_READS_MODE=2, UNIT_TESTS_MODE=3
-} MainMode;
-
-const char *modes[] = {"NN_CLASSIFIER_MODE", "KNN_CLASSIFIER_MODE", "RAW_READS_MODE", "UNIT_TESTS_MODE"};
-
-volatile MainMode mode;
 
 I2C_HandleTypeDef hi2c1;
 
@@ -87,101 +80,6 @@ void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin) {
 	}
 }
 
-int8_t interval_passed(uint32_t now, uint32_t prev, uint32_t interval) {
-	return now >= prev + interval;
-}
-
-
-void process_reads(uint32_t now, classifiers_dataset_t *dataset) {
-	const uint32_t results_update_frequency = 10;	// Hz
-	const uint32_t results_update_interval = 1000 / results_update_frequency; // ms
-	static uint32_t  previous_results_update = 0;
-	const int16_t code_no_result = -1;
-
-	if (dataset->is_ready
-			  && interval_passed(now, previous_results_update, results_update_interval)) {
-		previous_results_update = now;
-
-		char msgbuf[50] = { '\0' };
-		int16_t result_code;
-		static int16_t prev_result;
-
-		switch(mode) {
-		case NN_CLASSIFIER_MODE:
-			result_code = nn_classifier(dataset->series);
-
-			if(result_code != code_no_result && result_code != prev_result) {
-				const char *gesture = nn_get_name(result_code);
-				sprintf(msgbuf, "NN: [%lu] %s\r\n", now, gesture);
-				TM_USART_Puts(USART6, msgbuf);
-				prev_result = result_code;
-			}
-			break;
-
-		case KNN_CLASSIFIER_MODE:
-			result_code = knn_classifier(dataset->series);
-			if(result_code != code_no_result && result_code != prev_result) {
-				const char *gesture = knn_get_name(result_code);
-				sprintf(msgbuf, "KNN: [%lu] %s\r\n", now, gesture);
-				TM_USART_Puts(USART6, msgbuf);
-				prev_result = result_code;
-			}
-			break;
-
-		case UNIT_TESTS_MODE:
-			run_all_tests(USART6);
-			break;
-
-		default:
-			break;
-		}
-	}
-}
-
-MainMode find_mode(char msg) {
-	switch(msg) {
-	case 's':
-	case 'S':
-	case 'r':
-	case 'R':
-		return RAW_READS_MODE;
-		break;
-
-	case 'n':
-	case 'N':
-		return NN_CLASSIFIER_MODE;
-		break;
-
-	case 'k':
-	case 'K':
-	case 'd':
-	case 'D':
-		return KNN_CLASSIFIER_MODE;
-		break;
-
-	case 't':
-	case 'T':
-	case 'u':
-	case 'U':
-		return UNIT_TESTS_MODE;
-		break;
-
-	default:
-		return mode;
-		break;
-	}
-}
-
-void check_mode_switch(void) {
-	char c = TM_USART_Getc(USART6);
-	MainMode m = find_mode(c);
-	if(m != mode) {
-	  TM_USART_Puts(USART6, "Switching mode to ");
-	  TM_USART_Puts(USART6, (char *)modes[m]);
-	  TM_USART_Puts(USART6, "\r\n");
-	  mode = m;
-	}
-}
 /* USER CODE END PFP */
 
 /* USER CODE BEGIN 0 */
